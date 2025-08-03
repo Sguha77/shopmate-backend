78050dcb58fd84cbfa86662b29578e49a18a9d9b
Sguha77 <sguha1869@gmail.com>
2025-08-04 01:35:35 +0530

    changed env file

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getProducts, getProductById } = require('../controllers/productController');
 
+
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 
